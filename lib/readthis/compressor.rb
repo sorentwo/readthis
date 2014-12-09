40d5f75b790a5725a38d4b8bfd34d8bb8fb77224
Parker Selbert <parker@sorentwo.com>
2014-12-09 06:39:52 -0600

    Use the term threshold instead of minimum
    This is more consistent with the universal options from
    ActiveSupport::Cache

--- lib/readthis/compressor.rb
+++ lib/readthis/compressor.rb
@@ -2,22 +2,34 @@ require 'zlib'
 
 module Readthis
   class Compressor
-    attr_reader :minimum
+    attr_reader :threshold
 
-    def initialize(minimum: 1024)
-      @minimum = minimum
+    # Create a new Readthis::Compressor object that pivots on the provided
+    # threshold value.
+    #
+    # @param threshold [Number] the threshold size required for compression
+    def initialize(threshold: 1024)
+      @threshold = threshold
     end
 
+    # Compress a value if its size is greater or equal to the current threshold.
+    #
+    # @param value [String] a string to compress
     def compress(value)
-      if value.size >= minimum
+      if value.size >= threshold
         Zlib::Deflate.deflate(value)
       else
         value
       end
     end
 
+    # Decompress a previously compressed object. It will attempt to decode a
+    # value regardless of whether it has been compressed, but will rescue
+    # decoding errors.
+    #
+    # @param value [String] a possibly compressed string to decompress
     def decompress(value)
-      if value.size >= minimum
+      if value.size >= threshold
         Zlib::Inflate.inflate(value)
       else
         value
--- spec/readthis/compressor_spec.rb
+++ spec/readthis/compressor_spec.rb
@@ -3,15 +3,15 @@ require 'readthis/compressor'
 RSpec.describe Readthis::Compressor do
   describe '#compress' do
     it 'compresses the input' do
-      compressor   = Readthis::Compressor.new(minimum: 0)
+      compressor   = Readthis::Compressor.new(threshold: 0)
       input      = 'aaa bbb ccc'
       output     = compressor.compress(input)
 
       expect(input).not_to eq(output)
     end
 
-    it 'passes input below the minimum size through uncompressed' do
-      compressor = Readthis::Compressor.new(minimum: 1024)
+    it 'passes input below the threshold size through uncompressed' do
+      compressor = Readthis::Compressor.new(threshold: 1024)
       input      = 'abcdefg'
 
       expect(compressor.compress(input)).to eq(input)
@@ -20,7 +20,7 @@ RSpec.describe Readthis::Compressor do
 
   describe '#decompress' do
     it 'decompresses compressed data' do
-      compressor   = Readthis::Compressor.new(minimum: 0)
+      compressor   = Readthis::Compressor.new(threshold: 0)
       input        = 'aaa bbb ccc'
       compressed   = compressor.compress(input)
       decompressed = compressor.decompress(compressed)
@@ -29,7 +29,7 @@ RSpec.describe Readthis::Compressor do
     end
 
     it 'passes through decompression failures' do
-      compressor = Readthis::Compressor.new(minimum: 0)
+      compressor = Readthis::Compressor.new(threshold: 0)
       input      = 'abcdefg'
 
       expect(compressor.decompress(input)).to eq(input)
