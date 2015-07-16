0435d5c86ef39d132ed44cf9ffa8e297ec9c0e06
Parker Selbert <parker@sorentwo.com>
2015-07-16 18:00:03 -0500

    Update GC.stats options for Ruby 2.2

--- benchmarks/memory.rb
+++ benchmarks/memory.rb
@@ -1,11 +1,11 @@
 require 'bundler'; Bundler.setup
-a = GC.stat(:total_allocated_object)
+a = GC.stat(:total_allocated_objects)
 
 require 'readthis'
-b = GC.stat(:total_allocated_object)
+b = GC.stat(:total_allocated_objects)
 
 require 'redis-activesupport'
-c = GC.stat(:total_allocated_object)
+c = GC.stat(:total_allocated_objects)
 
 puts "readthis: #{b - a}"
 puts "redis-activesupport: #{c - b}"
