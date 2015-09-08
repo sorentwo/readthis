ffc82f4e4e2d9d1a40bf3b8e70cdce01d7a0131a
Parker Selbert <parker@sorentwo.com>
2015-09-08 17:06:59 -0500

    Introdce generic readthis benchmark
    Intended for comparing changes across branches and between commits.

---
+++ benchmarks/generic.rb
@@ -0,0 +1,34 @@
+require 'bundler'
+
+Bundler.setup
+
+require 'benchmark/ips'
+require 'readthis'
+require 'json'
+
+READTHIS = Readthis::Cache.new(
+  expires_in: 120,
+  marshal: JSON,
+  compress: true
+)
+
+def write_key(key)
+  READTHIS.write(key, key.to_s * 2048)
+end
+
+KEYS = (1..1_000).to_a
+KEYS.each { |key| write_key(key) }
+
+Benchmark.ips do |x|
+  x.report 'readthis:write' do
+    write_key(KEYS.sample)
+  end
+
+  x.report 'readthis:read' do
+    READTHIS.read(KEYS.sample)
+  end
+
+  x.report 'readthis:read_multi' do
+    READTHIS.read(KEYS.sample(30))
+  end
+end
