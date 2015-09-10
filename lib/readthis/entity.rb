35baf168ff8be55989db5294d34de8f7b38ad153
Parker Selbert <parker@sorentwo.com>
2015-09-10 09:23:58 -0500

    Add full documentation to entity methods

--- lib/readthis/entity.rb
+++ lib/readthis/entity.rb
@@ -20,20 +20,57 @@ module Readthis
     COMPRESSED_FLAG    = 0x8
     MARSHAL_FLAG       = 0x3
 
+    # Creates a Readthis::Entity with default options. Each option can be
+    # overridden later when entities are being dumped.
+    #
+    # Options are sticky, meaning that whatever is used when dumping will
+    # automatically be used again when loading, regardless of how current
+    # options are set.
+    #
+    # @option [Boolean] :compress (false) Enable or disable automatic compression
+    # @option [Module]  :marshal (Marshal) Any module that responds to `dump` and `load`
+    # @option [Number]  :threshold (8k) The size a string must be for compression
+    #
     def initialize(options = {})
       @options = DEFAULT_OPTIONS.merge(options)
     end
 
+    # Output a value prepared for cache storage. Passed options will override
+    # whatever has been specified for the instance.
+    #
+    # @param  [String] String to dump
+    # @option [Boolean] :compress Enable or disable automatic compression
+    # @option [Module]  :marshal Any module that responds to `dump` and `load`
+    # @option [Number]  :threshold The size a string must be for compression
+    # @return [String] The prepared, possibly compressed, string
+    #
+    # @example Dumping a value using defaults
+    #
+    #   entity.dump(string)
+    #
+    # @example Dumping a value with overrides
+    #
+    #   entity.dump(string, compress: false)
+    #
     def dump(value, options = {})
+      compress  = with_fallback(options, :compress)
       marshal   = with_fallback(options, :marshal)
       threshold = with_fallback(options, :threshold)
-      compress  = with_fallback(options, :compress)
 
       dumped = deflate(marshal.dump(value), compress, threshold)
 
       compose(dumped, marshal, compress)
     end
 
+    # Parse a dumped value using the embedded options.
+    #
+    # @param  [String] Option embedded string to load
+    # @return [String] The original dumped string, restored
+    #
+    # @example
+    #
+    #   entity.load(dumped)
+    #
     def load(string)
       marshal, compress, value = decompose(string)
 
@@ -49,6 +86,17 @@ module Readthis
     #
     # Where there are four unused bits, 1 compression bit, and 3 bits for the
     # serializer. This allows up to 8 different serializers for marshaling.
+    #
+    # @param  [String] String to prefix with flags
+    # @param  [Module] The marshal module to be used
+    # @param  [Boolean] Flag determining whether the value is compressed
+    # @return [String] The original string with a single byte prefixed
+    #
+    # @example Compose an option embedded string
+    #
+    #   entity.compose(string, Marshal, false) => 0x1  + string
+    #   entity.compose(string, JSON, true)     => 0x10 + string
+    #
     def compose(value, marshal, compress)
       flags  = SERIALIZER_FLAGS[marshal]
       flags |= COMPRESSED_FLAG if compress
@@ -56,6 +104,12 @@ module Readthis
       value.prepend([flags].pack('C'))
     end
 
+    # Decompose an option embedded string into marshal, compression and value.
+    #
+    # @param  [String] Option embedded string to
+    # @return [Array<Module, Boolean, String>] An array comprised of the
+    #   marshal, compression flag, and the base string.
+    #
     def decompose(string)
       flags = string[0].unpack('C').first
 
